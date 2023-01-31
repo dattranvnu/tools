30d5f308dc2fe8663df67d92b2143caea951c193
dattranvnu <dattranvnu@gmail.com>
2023-01-31 01:26:07 -0800

    new account

--- 1-html.py
+++ 1-html.py
@@ -6,8 +6,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 import time
 
 
-username = "wakamats@oregonstate.edu"
-password = "Baneyboo8"
+username = "CMR333221@yahoo.com"
+password = "Diamond3"
 output =''
 
 my_file = open("0-links.txt", "r")
@@ -27,16 +27,8 @@ i = 0
 div = ''
 for link in links:
     driver.get(link)
-    time.sleep(7)
+    time.sleep(10)
     # informationals & articles
-    try:
-      header = '## ' + driver.find_element(By.XPATH, '//div[@class="gamut-1ag67m1-FlexBox e1tc6bzh0"]/h1').text
-      nodes = driver.find_elements(By.XPATH, '//div[@class="gamut-1ag67m1-FlexBox e1tc6bzh0"]/*[name(.) !="h1"]')
-      for node in nodes:
-        div =''
-        div = div + node.get_attribute('innerHTML')
-    except:
-      pass
     try:
       header = '## ' + driver.find_element(By.XPATH, '//div[@class="gamut-1ag67m1-FlexBox e1tc6bzh0"]/h1').text
       nodes = driver.find_elements(By.XPATH, '//div[@class="gamut-1ag67m1-FlexBox e1tc6bzh0"]/*[name(.) !="h1"]')
@@ -49,8 +41,8 @@ for link in links:
     try:
       header = '## ' + driver.find_element(By.XPATH, '//div[@class="gamut-haybot-Text e8i0p5k0"]').text + '\n\n### ' + driver.find_element(By.XPATH, '//span[@class="gamut-yj8jvy-Text e8i0p5k0"]').text
       try:
-        div = driver.find_element(By.XPATH, '//div[@class="gamut-1s3gwqq-Box ebnwbv90"]/div[2]/div/div[2]').get_attribute('innerHTML')
-        nodes = driver.find_element(By.XPATH, '//div[@class="gamut-1s3gwqq-Box ebnwbv90"]/div[4]').get_attribute('innerHTML')
+        div = driver.find_element(By.XPATH, '//div[@class="gamut-iys7gh-Box ebnwbv90"]//div[2]/div/div[2]').get_attribute('innerHTML')
+        nodes = driver.find_element(By.XPATH, '//div[@class="gamut-iys7gh-Box ebnwbv90"]/div[4]').get_attribute('innerHTML')
         div = div + '\n\n\n\n\n' + '**Instructions** \n\n\n' + nodes  + '\n\n\n**Solutions**' + '\n\n\n```html\n\n```'
       except:
         pass
@@ -64,7 +56,7 @@ for link in links:
       pass
     # external_resources
     try:
-      header = '## ' + driver.find_element(By.XPATH, '//div[@class="gamut-xvi723-FlexBox e1tc6bzh0"]//h1').text
+      header = '## ' + driver.find_element(By.XPATH, '//div[@class="gamut-1qd5muv-FlexBox-ExternalResourceContainer e1xk5veq0"]//h1').text
       div = driver.find_element(By.XPATH, '//div[@class="gamut-1qd5muv-FlexBox-ExternalResourceContainer e1xk5veq0"]//*[name(.) !="h1"]').get_attribute('innerHTML')
     except:
       pass
