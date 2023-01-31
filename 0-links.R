b021df98dfc237d98101adfe6157aea320d556c6
dattranvnu <dattranvnu@gmail.com>
2023-01-31 00:31:35 -0800

    updates

--- 0-links.R
+++ 0-links.R
@@ -1,4 +1,5 @@
 library(rvest)
+library(tidyverse)
 links = paste(paste("https://www.codecademy.com", read_html("0-links.html") %>% html_nodes(xpath='//a') %>% html_attr('href'),sep=''),
 collapse=',\n')
 
