4afd1e8f748b6d4ab42a1060c03b046d56f6bb14
dattranvnu <dattranvnu@gmail.com>
2023-02-13 22:41:14 -0800

    Simplify

--- 0-links.R
+++
@@ -1,8 +0,0 @@
-library(rvest)
-library(tidyverse)
-links = paste(paste("https://www.codecademy.com", read_html("0-links.html") %>% html_nodes(xpath='//a') %>% html_attr('href'),sep=''),
-collapse=',\n')
-
-fileConn<-file("0-links.txt")
-writeLines(links, fileConn)
-close(fileConn)
---
+++ scrape.py
@@ -0,0 +1,69 @@
+import time
+
+from bs4 import BeautifulSoup
+from lxml import etree
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+wait = WebDriverWait(driver, 100)
+
+soup = BeautifulSoup(open('0-links.html'), 'html.parser')
+dom = etree.HTML(str(soup))
+links = [link.get('href') for link in soup.find_all('a')]
+links = [f"https://www.codecademy.com{link}" for link in links]
+
+links = ['https://www.codecademy.com/paths/build-python-web-apps-with-django/tracks/django-introduction-to-the-web/modules/django-introduction-to-the-web/informationals/django-welcome', 'https://www.codecademy.com/paths/build-python-web-apps-with-django/tracks/django-introduction-to-the-web/modules/django-introduction-to-the-web/articles/django-exploring-html-and-css', 'https://www.codecademy.com/paths/build-python-web-apps-with-django/tracks/introduction-to-django/modules/introduction-to-django/lessons/creating-your-first-django-app/exercises/configuring-django-settings']
+
+username, password, output = "CMR333221@yahoo.com", "Diamond3", ""
+
+
+driver.get("https://www.codecademy.com/login")
+wait.until(EC.element_to_be_clickable(driver.find_element(By.XPATH, '//*[@id="user_login"]'))).send_keys(username)
+wait.until(EC.element_to_be_clickable(driver.find_element(By.XPATH, '//*[@id="login__user_password"]'))).send_keys(password)
+wait.until(EC.element_to_be_clickable(driver.find_element(By.XPATH, '//button[@class="e1w6mdco0 gamut-18luxs6-ResetElement-createButtonComponent e1bhhzie0"]'))).click()
+time.sleep(8)
+i = 0
+instructions = ''
+# print(links)
+for link in links:
+    driver.get(link)
+    time.sleep(10)
+    try:
+        # informationals & articles
+        contents = driver.find_element(By.XPATH, '//div[@class="gamut-1ag67m1-FlexBox e1tc6bzh0"]').get_attribute("innerHTML")
+    except:
+        pass
+    try:
+        # lessons & projects
+        learn = driver.find_element(By.XPATH, '//div[@class="gamut-1s3gwqq-Box ebnwbv90"]/div[2]')
+        instructions = driver.find_element(By.XPATH, '//div[@class="gamut-1s3gwqq-Box ebnwbv90"]/div[4]')
+        contents = f'<h1>Learn</h1>\n\n\n{learn.get_attribute("innerHTML")}\n\n\n<h2>Instructions</h1>\n\n\n{instructions.get_attribute("innerHTML")}\n\n\n<h2>Solution</h2>\n\n\n```python\n\n```'
+    except:
+        pass
+    # # videos
+    # try:
+    #   learn = '## ' + driver.find_element(By.XPATH, '//div[@class="gamut-xvi723-FlexBox e1tc6bzh0"]//h1').text
+    #   instructions = driver.find_element(By.XPATH, '//main//iframe/..').get_attribute('innerHTML')
+    # except:
+    #   pass
+    # # external_resources
+    # try:
+    #   learn = '## ' + driver.find_element(By.XPATH, '//div[@class="gamut-1qd5muv-FlexBox-ExternalResourceContainer e1xk5veq0"]//h1').text
+    #   instructions = driver.find_element(By.XPATH, '//div[@class="gamut-1qd5muv-FlexBox-ExternalResourceContainer e1xk5veq0"]//*[name(.) !="h1"]').get_attribute('innerHTML')
+    # except:
+    #   pass
+
+    output = f"{output}\n\n\n\n\n{contents}"
+    print(i, link)
+    i += 1
+
+f=open("README.html","w")
+f.write(output)
+f.close()
